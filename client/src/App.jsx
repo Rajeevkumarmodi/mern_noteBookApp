3fb51b38b6018aaf8d1a0f4ee848e8b9036b5c34
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-10-02 11:45:41 +0530

    include rotected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
@@ -10,17 +10,21 @@ import NoPage from "./pages/noPage/NoPage";
 import ContextProvider from "./context/ContextProvider";
 
 function App() {
+  const token = localStorage.getItem("token");
   return (
     <div>
       <ContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={token ? <Home /> : <Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/addnote" element={<AddNote />} />
-            <Route path="/updatenote/:id" element={<UpdateNote />} />
+            <Route path="/profile" element={token ? <Profile /> : <Login />} />
+            <Route path="/addnote" element={token ? <AddNote /> : <NoPage />} />
+            <Route
+              path="/updatenote/:id"
+              element={token ? <UpdateNote /> : <Login />}
+            />
             <Route path="/*" element={<NoPage />} />
           </Routes>
         </BrowserRouter>
